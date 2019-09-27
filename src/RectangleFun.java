26788f7e25f313f389ab2c0820217d05650925e1
richaar <arianna.richardson@ssfs.org>
2019-09-27 12:50:48 -0400

    Rectangle Fun

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -6,6 +6,7 @@ public class RectangleFun {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Rectangle2D rec = new Rectangle();
 
         System.out.print("Width: ");
      double w = input.nextInt();
@@ -15,15 +16,11 @@ public class RectangleFun {
        double x = input.nextInt();
         System.out.print("Y: ");
       double y = input.nextInt();
-
-
-
-  Rectangle2D rec = new Rectangle();
-  rec.setRect(x - 4, y + 2, w, h);
    w = rec.getWidth();
    h = rec.getHeight();
-   double perimeter = w + h;
-   System.out.println("Perimeter =  " + perimeter);
+
+   System.out.println("Perimeter =  " + 2 * rec.getHeight() + 2 * rec.getWidth());
+   rec.setRect(x - 4, y + 2, w, h);
     x = rec.getX();
     y = rec.getY();
         System.out.print("New location is: ");
