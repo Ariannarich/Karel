a3d5f9766b2cbe8fd6238f597c2f02dca2791116
richaar <arianna.richardson@ssfs.org>
2019-10-03 10:17:17 -0400

    Fight Song

---
+++ src/FightSong.java
@@ -0,0 +1,34 @@
+public class FightSong {
+    static void statement1()
+    {
+System.out.println("Go, team, go! \n You can do it.");
+    }
+
+    static void statement2()
+    {
+        System.out.println("You're the best,\n" +
+                "In the West.\n");
+    }
+    static void newline()
+    {
+        System.out.println(" ");
+    }
+    public static void main(String[] args)
+    {
+statement1();
+newline();
+statement1();
+        statement2();
+        statement1();
+        newline();
+        statement1();
+        statement2();
+        statement1();
+        newline();
+        statement1();
+        statement2();
+        statement1();
+        newline();
+        statement1();
+    }
+}
---
+++ src/SeparatingDigits.java
@@ -0,0 +1,11 @@
+public class SeparatingDigits {
+    public static void main(String[] args) {
+        int num = 4562 / 1000;
+
+        System.out.println(num % 1000);
+        double num2 = 4562 / 1000;
+
+        System.out.println(num2 % 1000);
+
+    }
+}
