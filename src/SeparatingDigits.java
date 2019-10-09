f2c4777ee6009dce4b933ad03bedee447ca4b284
richaar <arianna.richardson@ssfs.org>
2019-10-09 13:45:09 -0400

    Separating Digits done

--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -2,27 +2,33 @@ import java.util.Scanner;
 
 public class SeparatingDigits
 {
+   public static int quotient(int a ) {
+        a /= 10;
+        return a;
+    }
+    public static int remainder(int b) {
+         b %= 10;
+            return b;
+        }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
         int num2 = quotient(num);
-        int num3 = remainder(num2);
-    }
-    static int quotient(int a ) {
-    {
-      a = a / 10;
-      return a;
-
-   }
-    static int remainder(int b) {
-    {
-       b = b % 10;
-       return b;
+        int dig3 = remainder(num2);
+        int num3 =quotient(num2);
+        int dig2 = remainder(num3);
+        int num4 =quotient(num3);
+        int dig1 = remainder(num4);
+        int dig4 = remainder(num);
+        System.out.println(  dig1 +  "  "+ dig2 + "  " + dig3 + "  " + dig4);
 
     }
-    public static void main(String[] args) {
-
-
-
     }
-}
+
+
+
+
+
+
+
