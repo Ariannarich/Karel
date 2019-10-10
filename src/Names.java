6502e6b5e75edba6bfb72da0eb8e5e8b7e106008
Ariannarich <arianna.richardson@ssfs.org>
2019-10-10 18:12:24 -0400

    Names done

--- src/Names.java
+++ src/Names.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
 
-public class Names {
+public class
+Names {
 
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.println("Enter first name here");
     String first = input.next();
+        System.out.println("Enter last name here");
         String last = input.next();
         int tot= first.length() + last.length();
-        int f = first.indexOf(0);
+         String f= first.substring(0,1);
+        String l= last.substring(0,1);
+        int half = first.length() /2;
+        int half2 = last.length() / 2;
+        String lh = last.substring(2);
         System.out.println(tot);
-        System.out.println(f);
+        System.out.println(f+l);
+        System.out.println(first.compareTo(last));
+        System.out.println(last.lastIndexOf(first));
+        System.out.println(first.lastIndexOf(last));
+        System.out.print(first.substring(0, half2) + lh);
+        System.out.println("  " + last.substring(0, half) + first.substring(2));
+
+
 
 
 
