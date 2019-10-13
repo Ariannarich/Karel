460aefb08fb670da30d9843173d146613946659a
Ariannarich <arianna.richardson@ssfs.org>
2019-10-12 20:34:21 -0400

    Wrapper class done

--- src/WrappersDelight.java
+++ src/WrappersDelight.java
@@ -1,28 +1,34 @@
-public class WrappersDelight {
-    int num1 = 14;
-    Integer num2 = new Integer(8);
-    double num3 = 2.5;
-    double num4 = num2 + num3;
 
-    public int getNum1(double num1) {
-        num1 = (double)num2 + num4;
-        System.out.println(num1);
-        return (int) num1;
-    }
-    public Integer getNum2(int num2) {
-        num2 = 5;
-        System.out.println(num2);
-        return num2;
-    }
-    public double getNum3(double  num3) {
+public class WrappersDelight
+{
+
+
+
+    public static void main(String[] args) {
+        int num1 = 14;
+        Integer num2 = new Integer(8);
+        double num3 = 2.5;
+        Double num4 = new Double(num2 + num3);
+
+        num2 = new Integer(5);
         num3 = num1 + num2 + num4;
+        num1 = (int) (num2 + num4);
+
+        System.out.println(num1);
+
+        System.out.println(num2);
+
+
+
         System.out.println(num3);
+
+
         System.out.println(num4);
-        return num3;
-    }
+
+
+}
+}
 
 
 
-
-    }
 
