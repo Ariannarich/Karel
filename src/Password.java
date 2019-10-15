e96d7287b83d25fa722ff35dababb4065d16b67c
Ariannarich <arianna.richardson@ssfs.org>
2019-10-15 07:32:20 -0400

    pass not done

--- src/Password.java
+++ src/Password.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class Password {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter first name here");
         String first = input.next();
+        System.out.println("Enter middle name here");
         String middle = input.next();
+        System.out.println("Enter last name here");
         String last = input.next();
+        System.out.println("Enter age here");
         int age = input.nextInt();
         int fm = first.length() /2;
         int mm = middle.length() /2;
         int lm = last.length() /2;
+ int mid= fm +mm +lm;
+ int las = first.lastIndexOf(first) + middle.lastIndexOf(middle) + middle.lastIndexOf(middle);
+  int newAge = age * 75;
+
+
+        System.out.println("Password");
+        System.out.print(first.substring(fm)        System.out.print(las+ newAge);
+
     }
 
 
