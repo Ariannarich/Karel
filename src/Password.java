292adf8a4a831fb1e84848115ae0f60105db3ffd
richaar <arianna.richardson@ssfs.org>
2019-10-15 09:00:45 -0400

    Pass not finished

--- src/Password.java
+++ src/Password.java
@@ -14,13 +14,18 @@ public class Password {
         int fm = first.length() /2;
         int mm = middle.length() /2;
         int lm = last.length() /2;
- int mid= fm +mm +lm;
- int las = first.lastIndexOf(first) + middle.lastIndexOf(middle) + middle.lastIndexOf(middle);
+String mid = first.substring(fm,fm+1) + middle.substring(mm,mm+1) + last.substring(lm, lm+1);
+ int lasf = first.lastIndexOf(first);
+int lasm = middle.lastIndexOf(middle);
+ int lasl = last.lastIndexOf(last);
+ String laslet =  first.substring(lasf-1,lasf) + middle.substring(lasm) + last.substring(lasl);
   int newAge = age * 75;
 
 
         System.out.println("Password");
-        System.out.print(first.substring(fm)        System.out.print(las+ newAge);
+        System.out.print(mid.toUpperCase());
+      System.out.println(laslet);
+        System.out.print(newAge);
 
     }
 
