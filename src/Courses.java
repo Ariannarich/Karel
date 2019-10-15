928ccaca5fdbd2bcfef5ee2806902dd1e266a116
Ariannarich <arianna.richardson@ssfs.org>
2019-10-15 07:56:18 -0400

    courses not done

---
+++ src/Courses.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Courses {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+      String title = input.next();
+      int titleLength =  title.indexOf(title);
+      int department = title.indexOf(titleLength,0);
+
+
+
+
+
+}
