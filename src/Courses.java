01e8ae42622c2cf496254d6e8e33d70db9a41b03
Ariannarich <arianna.richardson@ssfs.org>
2019-10-16 22:36:33 -0400

    Password Done

--- src/Courses.java
+++ src/Courses.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class Courses {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-      String title = input.next();
-      int titleLength =  title.indexOf(title);
-      int department = title.indexOf(titleLength,0);
+        String title = input.next();
+        int titleLength = title.indexOf(title);
+        int department = title.indexOf(titleLength, 0);
 
 
-
-
-
+    }
 }
--- src/Password.java
+++ src/Password.java
@@ -13,14 +13,24 @@ public class Password {
         int age = input.nextInt();
         int fm = first.length() /2;
         int mm = middle.length() /2;
-        int lm = last.length() /2;
- int mid= fm +mm +lm;
- int las = first.lastIndexOf(first) + middle.lastIndexOf(middle) + middle.lastIndexOf(middle);
-  int newAge = age * 75;
+        int ml = last.length() /2;
+ String beg= first.substring(fm,fm+1) + middle.substring(mm,mm+1)  + last.substring(ml,ml+1);
+int f = first.length() - 1;
+int m = middle.length() - 1;
+int l = last.length() - 1;
+String ll = last.substring(l) +middle.substring(m) + first.substring(f);
+
+        int newAge = age * 75;
+  String age2 = String.valueOf(newAge);
+
+         beg= beg.toUpperCase();
+        String pass =beg.concat(ll).concat(age2);
 
 
-        System.out.println("Password");
-        System.out.print(first.substring(fm)        System.out.print(las+ newAge);
+
+                System.out.println("Password");
+        System.out.print(pass);
+
 
     }
 
